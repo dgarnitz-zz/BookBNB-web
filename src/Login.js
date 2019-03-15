70ebb31b80a8aec5f9846e7d7f72758633dc1e3a
dgarnitz <david.garnitz@gmail.com>
2019-03-15 18:17:34 +0000

    created project template and added a simply login form that does not work yet

---
+++ src/Login.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl'
+
+class Login extends Component {
+  render() {
+    return (
+      <div className = "login-wrapper">
+        <Typography variant="h5" paragraph={true} className="login-prompt">
+          Please Log In!
+        </Typography>
+        <form className="login-form">
+          <FormControl margin='dense'>
+            <TextField type="text" label="Email:" />
+          </FormControl>
+          <FormControl margin='dense'>
+            <TextField type="text" label="Passport:" />
+          </FormControl>
+          <FormControl margin='dense'>
+            <Button variant="contained" color="primary">
+              Login
+            </Button>
+          </FormControl>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
