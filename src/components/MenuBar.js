761fadabedd8211fef0d1d5e95cf74a732f9b754
antondubek <makepeaceanthony@gmail.com>
2019-03-24 12:09:57 +0700

    Added improvements to menubar including different bar for logged in and not logged in. Also changed to navlinks so that the right header is highlighted when selected

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 class MenuBar extends React.Component {
 
-    render(){
+    state = {loggedIn: false};
+
+    ConditionalRender = () => {
+        if (this.state.loggedIn){
+            return (
+                <div className="ui teal secondary pointing menu">
+                    <NavLink to="/mybooks" className="item">My Books</NavLink>
+                    <NavLink to="/books" className="item">Browse</NavLink>
+                    <div className="right menu" style={{marginTop: 10, marginBottom: 5}}>
+                        <div class="ui labeled icon buttons mini">
+                            <button class="ui primary basic button"><i class="user icon"></i>Profile</button>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        
         return (
             <div className="ui teal secondary pointing menu">
-                <Link to="/books" className="item">Browse</Link>
-                <div className="right menu">
-                    <Link to="/login"><button className="ui primary basic small button">Register</button></Link>
-                    <Link to="/login"><button className="ui basic small button">Login</button></Link>
+                <NavLink to="/books" className="item">Browse</NavLink>
+                <div className="right menu" style={{marginTop: 10, marginBottom: 5}}>
+                    <NavLink to="/login"><button className="ui primary basic mini button">Register</button></NavLink>
+                    <NavLink to="/login"><button className="ui basic mini button">Login</button></NavLink>
                 </div>
             </div>
         );
+        
+    }
+
+    render(){
+        return this.ConditionalRender();
     }
 }
 
