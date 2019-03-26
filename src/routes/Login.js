7c231077e94e6888485790675385a800974cf007
antondubek <makepeaceanthony@gmail.com>
2019-03-26 11:46:52 +0700

    Added in SHA1 hashing for password, works correctly with server

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,53 +1,63 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import '../App.css';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import FormControl from '@material-ui/core/FormControl';
+import React, { Component } from "react";
+import axios from "axios";
+import "../App.css";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
 
 class Login extends Component {
-  constructor(props){
-   super(props);
+  constructor(props) {
+    super(props);
 
-   this.login = this.login.bind(this);
-   this.sampleData = {email: 'garnizzle@garnizzle.com',
-                password: 'garnizzle'};
+    this.login = this.login.bind(this);
+    this.sampleData = {
+      email: "garnizzle@garnizzle.com",
+      password: "garnizzle"
+    };
   }
 
   login(event) {
-    axios.post(`${'https://cors-anywhere.herokuapp.com/'}https://antondubek-bookbnb.herokuapp.com/login`, {
-    email: event.target.email.value,
-    password: event.target.password.value
-    })
-    .then(response => {
-      console.log(response);
-    })
-    .catch(error => {
-      console.log(error);
-    });
     event.preventDefault();
+
+    var hashPassword = require("crypto")
+      .createHash("SHA1")
+      .update(event.target.password.value, "utf-8")
+      .digest("hex");
+
+    console.log(hashPassword);
+
+    axios
+      .post(
+        `${"https://cors-anywhere.herokuapp.com/"}https://antondubek-bookbnb.herokuapp.com/login`,
+        {
+          email: event.target.email.value,
+          password: hashPassword
+        }
+      )
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
     return (
-      <div className = "login-wrapper">
+      <div className="login-wrapper">
         <Typography variant="h5" paragraph={true} className="login-prompt">
           Please Log In!
         </Typography>
-        <form className="login-form" onSubmit = {this.login}>
-          <FormControl margin='dense'>
+        <form className="login-form" onSubmit={this.login}>
+          <FormControl margin="dense">
             <TextField type="text" label="Email:" name="email" />
           </FormControl>
-          <FormControl margin='dense'>
+          <FormControl margin="dense">
             <TextField type="text" label="Password:" name="password" />
           </FormControl>
-          <FormControl margin='dense'>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-            >
+          <FormControl margin="dense">
+            <Button type="submit" variant="contained" color="primary">
               Login
             </Button>
           </FormControl>
