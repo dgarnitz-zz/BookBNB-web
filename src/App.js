92b3256cb787ad0ffd802cd943b560d232831bcb
dgarnitz <david.garnitz@gmail.com>
2019-03-28 17:10:29 +0000

    fixed errors resulting from merge

--- src/App.js
+++ src/App.js
@@ -2,35 +2,24 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./routes/Login";
 import BookList from "./routes/BookList";
-<<<<<<< HEAD
 import Followers from "./routes/Followers";
-=======
 import MenuBar from "./components/MenuBar";
 import Profile from "./routes/Profile";
->>>>>>> master
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Router>
-<<<<<<< HEAD
-        <Switch>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={RegisterForm} />
-          <Route exact path="/book" component={BookList} />
-          <Route exact path="/followers" component={Followers} />
-        </Switch>
-=======
         <div className="container">
           <MenuBar />
           <Switch>
             <Route exact path="/login" component={Login} />
             <Route exact path="/book" component={BookList} />
             <Route exact path="/profile" component={Profile} />
+            <Route exact path="/followers" component={Followers} />
           </Switch>
         </div>
->>>>>>> master
       </Router>
     );
   }
