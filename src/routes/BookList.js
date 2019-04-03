5741cb3df801bb796d4d7ad714081c216fa30bb4
dgarnitz <david.garnitz@gmail.com>
2019-04-03 11:36:47 +0100

    fixing merge conflicts

--- src/routes/BookList.js
+++ src/routes/BookList.js
@@ -56,22 +56,6 @@ class BookList extends Component {
                 />
               );
             })}
-<<<<<<< HEAD
-=======
-            <TableRow>
-              <TableCell>
-                <Fab>
-                  <AddIcon/>
-                  <a href="/addbook" > test </a>
-                </Fab>
-              </TableCell>
-              <TableCell>
-                <Fab>
-                  <DeleteIcon />
-                </Fab>
-              </TableCell>
-            </TableRow>
->>>>>>> master
           </TableBody>
         </Table>
       </Paper>
