6f26d2dc01698a08fc8d1e21e188d014dab8f500
antondubek <makepeaceanthony@gmail.com>
2019-04-12 16:36:04 +0100

    Made some work adding some icons with the data in profile but still needs some work

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -9,170 +9,166 @@ import classNames from "classnames";
 import { read_cookie } from "sfcookies";
 
 const styles = {
-	root: {
-		flexGrow: 1
-	}
+  root: {
+    flexGrow: 1
+  }
 };
 
 class MenuBar extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+  heading(classes) {
+    return (
+      <NavLink to="/book">
+        <Button size="medium" className={classes.button} size="large">
+          <BookIcon
+            className={classNames(classes.leftIcon, classes.iconMedium)}
+            style={{
+              margin: 3
+            }}
+          />
+          BookBnB
+          <BookIcon
+            className={classNames(classes.rightIcon, classes.iconMedium)}
+            style={{
+              margin: 3
+            }}
+          />
+        </Button>
+      </NavLink>
+    );
+  }
 
-	heading(classes) {
-		return (
-			<NavLink to="/book">
-				<Button size="medium" className={classes.button} size="large">
-					<BookIcon
-						className={classNames(classes.leftIcon, classes.iconMedium)}
-						style={{
-							margin: 3
-						}}
-					/>
-					BookBnB
-					<BookIcon
-						className={classNames(classes.rightIcon, classes.iconMedium)}
-						style={{
-							margin: 3
-						}}
-					/>
-				</Button>
-			</NavLink>
-		);
-	}
+  browseBtn(classes) {
+    return (
+      <NavLink to="/book">
+        <Button
+          color="default"
+          variant="outlined"
+          className={classes.button}
+          size="large"
+        >
+          Browse
+        </Button>
+      </NavLink>
+    );
+  }
 
-	browseBtn(classes) {
-		return (
-			<NavLink to="/book">
-				<Button
-					color="default"
-					variant="outlined"
-					className={classes.button}
-					size="large"
-				>
-					Browse
-				</Button>
-			</NavLink>
-		);
-	}
+  logout(event) {
+    event.preventDefault();
+  }
 
-	logout(event) {
-		event.preventDefault();
-	}
+  render() {
+    const { classes } = this.props;
 
-	render() {
-		const { classes } = this.props;
+    var loggedIn = false;
 
-		var loggedIn = false;
+    if (typeof read_cookie("email") === "string") {
+      loggedIn = true;
+    }
 
-		if (typeof read_cookie("email") === "string") {
-			loggedIn = true;
-		}
+    if (loggedIn) {
+      return (
+        <div className={classes.root}>
+          <AppBar position="static" color="default">
+            <Toolbar>
+              <Grid container>
+                <Grid> {this.heading(classes)} </Grid>
+                <Grid> {this.browseBtn(classes)} </Grid>
+                <Grid>
+                  <NavLink to="/mybooks">
+                    <Button
+                      color="default"
+                      className={classes.button}
+                      variant="outlined"
+                      size="large"
+                    >
+                      MyBooks
+                    </Button>
+                  </NavLink>
+                </Grid>
+              </Grid>
+              <Grid>
+                <NavLink to="/profile">
+                  <Button
+                    variant="outlined"
+                    size="large"
+                    color="primary"
+                    className={classes.button}
+                  >
+                    <AccountIcon
+                      className={classNames(
+                        classes.leftIcon,
+                        classes.iconMedium
+                      )}
+                      style={{
+                        marginRight: 5
+                      }}
+                    />
+                    Profile
+                  </Button>
+                </NavLink>
+              </Grid>
+              <Grid>
+                <NavLink to="/login">
+                  <Button
+                    variant="outlined"
+                    size="large"
+                    className={classes.button}
+                    onClick={this.props.logout}
+                  >
+                    Logout
+                  </Button>
+                </NavLink>
+              </Grid>
+            </Toolbar>
+          </AppBar>
+        </div>
+      );
+    }
 
-		if (loggedIn) {
-			return (
-				<div className={classes.root}>
-					<AppBar position="static" color="default">
-						<Toolbar>
-							<Grid container>
-								<Grid> {this.heading(classes)} </Grid>
-								<Grid> {this.browseBtn(classes)} </Grid>
-								<Grid>
-									<NavLink to="/mybooks">
-										<Button
-											color="default"
-											className={classes.button}
-											variant="outlined"
-											size="large"
-										>
-											MyBooks
-										</Button>
-									</NavLink>
-								</Grid>
-							</Grid>
-							<Grid>
-								<NavLink to="/profile">
-									<Button
-										variant="outlined"
-										size="large"
-										color="primary"
-										className={classes.button}
-									>
-										<AccountIcon
-											className={classNames(
-												classes.leftIcon,
-												classes.iconMedium
-											)}
-											style={{
-												marginRight: 5
-											}}
-										/>
-										Profile
-									</Button>
-								</NavLink>
-							</Grid>
-							<Grid>
-								<NavLink to="/login">
-									<Button
-										variant="outlined"
-										size="large"
-										className={classes.button}
-										onClick={this.props.logout}
-									>
-										Logout
-									</Button>
-								</NavLink>
-							</Grid>
-						</Toolbar>
-					</AppBar>
-				</div>
-			);
-		}
-
-		return (
-			<div>
-				<AppBar position="static" color="default">
-					<Toolbar>
-						<Grid container>
-							<Grid> {this.heading(classes)} </Grid>
-							<Grid> {this.browseBtn(classes)} </Grid>{" "}
-						</Grid>
-						<Grid>
-							<NavLink to="/login">
-								<Button
-									variant="outlined"
-									size="large"
-									color="primary"
-									className={classes.button}
-								>
-									Register
-								</Button>
-							</NavLink>
-						</Grid>
-						<Grid>
-							<NavLink to="/login">
-								<Button
-									variant="outlined"
-									size="large"
-									color="default"
-									className={classes.button}
-									style={{
-										margin: 5
-									}}
-								>
-									Login
-								</Button>
-							</NavLink>
-						</Grid>
-					</Toolbar>
-				</AppBar>
-			</div>
-		);
-	}
+    return (
+      <div>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Grid container>
+              <Grid> {this.heading(classes)} </Grid>
+              <Grid> {this.browseBtn(classes)} </Grid>{" "}
+            </Grid>
+            <Grid>
+              <NavLink to="/login">
+                <Button
+                  variant="outlined"
+                  size="large"
+                  color="primary"
+                  className={classes.button}
+                >
+                  Register
+                </Button>
+              </NavLink>
+            </Grid>
+            <Grid>
+              <NavLink to="/login">
+                <Button
+                  variant="outlined"
+                  size="large"
+                  color="default"
+                  className={classes.button}
+                  style={{
+                    margin: 5
+                  }}
+                >
+                  Login
+                </Button>
+              </NavLink>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
 MenuBar.propTypes = {
-	classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(MenuBar);
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,8 +1,20 @@
 import React from "react";
 import axios from "axios";
-import ProfileBooks from './../components/ProfileBooks';
+import ProfileBooks from "./../components/ProfileBooks";
 import { read_cookie } from "sfcookies";
+import HomeIcon from "@material-ui/icons/HomeRounded";
+import EmailIcon from "@material-ui/icons/Email";
+import UserIcon from "@material-ui/icons/VerifiedUser";
+import { Grid, Typography } from "@material-ui/core";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 
+const styles = {
+  root: {
+    flexGrow: 1
+  }
+};
 class Profile extends React.Component {
   state = { email: "", name: "", city: "" };
 
@@ -11,7 +23,6 @@ class Profile extends React.Component {
   }
 
   getUserData = () => {
-
     var email = read_cookie("email");
 
     axios
@@ -34,16 +45,84 @@ class Profile extends React.Component {
       });
   };
 
-  render() {
+  displayName(classes) {
+    return (
+      <Grid container direction="row" alignItems="center">
+        <Grid item>
+          <UserIcon
+            color="primary"
+            className={classNames(classes.leftIcon)}
+            fontSize="large"
+          />
+        </Grid>
+        <Grid item>
+          <Typography color="default" variant="h6">
+            {this.state.name}
+          </Typography>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  displayEmail(classes) {
     return (
       <div>
-        <div>Name: {this.state.name}</div>
-        <div>email: {this.state.email}</div>
-        <div>City: {this.state.city}</div>
+        <Grid container direction="row" alignItems="center">
+          <Grid item>
+            <EmailIcon
+              color="primary"
+              className={classNames(classes.leftIcon)}
+              fontSize="large"
+            />
+          </Grid>
+          <Grid item>
+            <Typography color="default" variant="h6">
+              {this.state.email}
+            </Typography>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+
+  displayCity(classes) {
+    return (
+      <Grid container direction="row" alignItems="center">
+        <Grid item>
+          <HomeIcon
+            color="primary"
+            className={classNames(classes.leftIcon)}
+            fontSize="large"
+          />
+        </Grid>
+        <Grid item>
+          <Typography color="default" variant="h6">
+            {this.state.city}
+          </Typography>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <Grid>Profile Page</Grid>
+        <Grid container>
+          <Grid> {this.displayName(classes)} </Grid>
+          <Grid> {this.displayCity(classes)} </Grid>
+          <Grid> {this.displayEmail(classes)} </Grid>
+        </Grid>
         <ProfileBooks />
       </div>
     );
   }
 }
 
-export default Profile;
+Profile.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Profile);
