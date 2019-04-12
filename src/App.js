c73f5e07cc37a36ae292af9b3cfa8128eda35dc1
antondubek <makepeaceanthony@gmail.com>
2019-04-12 12:24:20 +0100

    Logout button implemented and hooked up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect
 } from "react-router-dom";
 import "./App.css";
 import Login from "./routes/Login";
@@ -13,44 +13,52 @@ import MenuBar from "./components/MenuBar";
 import Profile from "./routes/Profile";
 import AddBook from "./routes/AddBook";
 import "./App.css";
-import { bake_cookie } from "sfcookies";
+import { bake_cookie, delete_cookie } from "sfcookies";
 
 class App extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = { defaultRoute: "/login" };
-    this.login = this.login.bind(this);
-  }
+		this.state = { defaultRoute: "/login" };
+		this.login = this.login.bind(this);
+		this.logout = this.logout.bind(this);
+	}
 
-  login(email) {
-    console.log("Login called");
-    bake_cookie("email", email);
-    this.forceUpdate();
-    this.setState({ defaultRoute: "/profile" });
-  }
+	login(email) {
+		console.log("Login called");
+		bake_cookie("email", email);
+		this.forceUpdate();
+		this.setState({ defaultRoute: "/profile" });
+	}
 
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <MenuBar />
-          <Switch>
-            <Route
-              exact
-              path="/login"
-              render={props => <Login {...props} email={this.login} />}
-            />
-            <Route exact path="/book" component={BookList} />
-            <Route exact path="/profile" component={Profile} />
-            <Route exact path="/followers" component={Followers} />
-            <Route exact path="/addbook" component={AddBook} />
-          </Switch>
-          <Redirect from="/" to={this.state.defaultRoute} />
-        </div>
-      </Router>
-    );
-  }
+	logout() {
+		console.log("Logout called");
+		delete_cookie("email");
+		this.forceUpdate();
+		this.setState({ defaultRoute: "/login" });
+	}
+
+	render() {
+		return (
+			<Router>
+				<div className="container">
+					<MenuBar logout={this.logout} />
+					<Switch>
+						<Route
+							exact
+							path="/login"
+							render={props => <Login {...props} email={this.login} />}
+						/>
+						<Route exact path="/book" component={BookList} />
+						<Route exact path="/profile" component={Profile} />
+						<Route exact path="/followers" component={Followers} />
+						<Route exact path="/addbook" component={AddBook} />
+					</Switch>
+					<Redirect from="/" to={this.state.defaultRoute} />
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
